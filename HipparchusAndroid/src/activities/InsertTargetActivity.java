686e289e8136a7a17b75e7a7fcf45935a420b577
mandimastro <mandimastro@450fd8fa-4023-bf2e-84ac-ac8650e4d450>
2012-12-11 16:22:33 +0000

    update layout format removed wheel views

--- HipparchusAndroid/src/activities/InsertTargetActivity.java
+++ HipparchusAndroid/src/activities/InsertTargetActivity.java
@@ -21,40 +21,6 @@ public class InsertTargetActivity extends Activity {
 		
 		final TextView raLabel = (TextView)findViewById(R.id.insertRaLbl);
 		
-		final WheelView raHour = (WheelView) findViewById(R.id.ra_h);
-		raHour.setViewAdapter(new NumericWheelAdapter(this, 0, 23));
-	
-		final WheelView raMins = (WheelView) findViewById(R.id.ra_min);
-		raMins.setViewAdapter(new NumericWheelAdapter(this, 0, 59, "%02d"));
-		raMins.setCyclic(true);
-		
-		final TextView decLabel = (TextView)findViewById(R.id.insertDecLbl);
-		
-		final WheelView decDeg = (WheelView) findViewById(R.id.ra_h);
-		decDeg.setViewAdapter(new NumericWheelAdapter(this, -90, 90));
-	
-		final WheelView decMin = (WheelView) findViewById(R.id.ra_min);
-		decMin.setViewAdapter(new NumericWheelAdapter(this, 0, 59, "%02d"));
-		decMin.setCyclic(true);
-		
 		Button save = (Button)findViewById(R.id.saveTargetCoord);
-		
-		OnWheelScrollListener scrollListener = new OnWheelScrollListener() {
-			public void onScrollingStarted(WheelView wheel) {
-				raScrolled = true;
-			}
-			public void onScrollingFinished(WheelView wheel) {
-				raScrolled = false;
-				raChanged = true;
-				//picker.setCurrentHour(hours.getCurrentItem());
-				System.out.print(raHour.getCurrentItem()+"h");
-				System.out.println(raMins.getCurrentItem()+"m");
-				//picker.setCurrentMinute(mins.getCurrentItem());
-				raChanged = false;
-			}
-		};
-		
-		raHour.addScrollingListener(scrollListener);
-		raMins.addScrollingListener(scrollListener);
 	}
 }
